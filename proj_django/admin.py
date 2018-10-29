0072718e1a0d084c76dcdce857c1762be389c787
Muzque <zerosky1943@gmail.com>
2018-10-30 07:42:20 +0800

    remove pycharm folder

--- proj_django/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from recommend.models import Album
-from recommend.models import Song
-
-# Register your models here.
-admin.site.register(Album)
-admin.site.register(Song)
--- recommend/admin.py
+++ recommend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from recommend.models import Album
+from recommend.models import Song
 
+admin.site.register(Album)
+admin.site.register(Song)
 
