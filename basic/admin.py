310d104368c055ceab41edbef09adcfe302f0c04
Muzque <zerosky1943@gmail.com>
2018-11-02 17:46:09 +0800

    initial superuser

--- basic/admin.py
+++ basic/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from configparser import ConfigParser
 from .models import UserProfile
 from .models import AppInfo
 from .models import Comment
+
+# Create superuser
+cfg = ConfigParser()
+cfg.read('private/envs.cfg')
+User.objects.create_superuser(username=cfg.get('Model', 'SUPERUSER_ROOT'),
+                              password=cfg.get('Model', 'SUPERUSER_PASSWORD'),
+                              email=cfg.get('Model', 'SUPERUSER_EMAIL'))
 
 # Register your models here.
 admin.site.register(UserProfile)
